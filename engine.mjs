cdf55572b5845c5fd44d8e0606ea13b4ed8cbae1
yKicchan <r7@ykicchan.com>
2024-12-25 21:17:35 +0900

    feat: marker と ins に対応

--- engine.mjs
+++ engine.mjs
@@ -1,14 +1,18 @@
 import container from 'markdown-it-container';
-import prism from 'markdown-it-prism';
 import attrs from 'markdown-it-attrs';
+import ins from 'markdown-it-ins';
+import mark from 'markdown-it-mark'
+import prism from 'markdown-it-prism';
 import textualUml from 'markdown-it-textual-uml';
 
 /** 
  * @type {import('@marp-team/marp-cli').Config<typeof import('@marp-team/marpit').Marpit>["engine"]}
  */
 export default ({ marp }) => marp
-  .use(prism)
   .use(textualUml)
+  .use(prism)
+  .use(mark)
+  .use(ins)
   .use(attrs)
   .use(container, '_')
   .use(container, 'c')
